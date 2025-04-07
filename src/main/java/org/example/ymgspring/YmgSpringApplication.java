2256218bf854b836aa5d6f56223853ff51ebd626
yarenibis <yarenibis150@gmail.com>
2025-04-07 22:28:13 +0300

    Initial commit

---
+++ src/main/java/org/example/ymgspring/YmgSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.ymgspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmgSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmgSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ymgspring/YmgSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ymgspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmgSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
