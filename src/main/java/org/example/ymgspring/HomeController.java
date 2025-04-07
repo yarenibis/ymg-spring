84e3cc06853e934c1d4e3f70fb117f55d8968ff4
yarenibis <yarenibis150@gmail.com>
2025-04-07 22:45:41 +0300

    ööönn

---
+++ src/main/java/org/example/ymgspring/HomeController.java
@@ -0,0 +1,14 @@
+// HomeController.java
+package org.example.ymgspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Başarıyla Çalışıyor 🚀";
+    }
+}
--- src/main/java/org/example/ymgspring/YmgSpringApplication.java
+++ src/main/java/org/example/ymgspring/YmgSpringApplication.java
@@ -1,19 +1,13 @@
+// YmgSpringApplication.java
 package org.example.ymgspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class YmgSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(YmgSpringApplication.class, args);
     }
-
-    @GetMapping("/")
-    public String home() {
-        return "Spring Boot Başarıyla Çalışıyor 🚀";
-    }
-
 }
+
